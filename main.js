095afc2b28df99f297a60b32cd82e4175cd6a36f
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2022-12-11 11:26:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY= 0;
 
 function setup(){
     canvas = createCanvas(600, 500);
@@ -6,6 +10,13 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet Is Initialized");
 }
 
 function draw(){
@@ -18,4 +29,20 @@ function preload(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX" + leftWristX + "leftWristY" + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX" + rightWristX + "rightWristY" + rightWristY);
+    }
 }
