d96531472c6ec09c4c39edb8efafc7556cbc916a
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2022-12-17 16:57:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,6 +21,19 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF000");
+
+    if(scoreLeftWrist > 0.2){
+    circle(leftWristX, leftWristY, 20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    leftWristY_divide_1000 = remove_decimals/1000;
+    volume = leftWristY_divide_1000 *2;
+    document.getElementById("volume").innerHTML = "Volume = " + volume;
+    song.setVolume(volume);
+    }
 }
 
 function preload(){
@@ -37,6 +50,9 @@ function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX" + leftWristX + "leftWristY" + leftWristY);
