85dceed5483039c10586aacfbcd4bc2da4be9560
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2022-12-18 11:40:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 song = "";
+scoreLeftWrist = 0;
+
 leftWristX = 0;
 leftWristY = 0;
+
 rightWristX = 0;
 rightWristY= 0;
 
@@ -23,9 +26,38 @@ function draw(){
     image(video, 0, 0, 600, 500);
 
     fill("#FF0000");
-    stroke("#FF000");
+    stroke("#FF0000");
 
-    if(scoreLeftWrist > 0.2){
+    circle(rightWristX, rightWristY, 20);
+
+    if(rightWristY > 0 && rightWristY <= 100)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY > 100 && rightWristY <= 200)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY > 200 && rightWristY <= 300)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY > 300 && rightWristY <= 400)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY > 400 && rightWristY <= 500)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5);
+    }
+
+    if(scoreLeftWrist > 0.2)
+    {
     circle(leftWristX, leftWristY, 20);
     InNumberleftWristY = Number(leftWristY);
     remove_decimals = floor(InNumberleftWristY);
@@ -50,8 +82,10 @@ function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log("scoreLeftWrist = " + scoreLeftWrist);
+        console.log("scoreRightWrist" + scoreRightWrist + "scoreLeftWrist" + scoreLeftWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
